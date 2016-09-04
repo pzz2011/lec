6a47713fc5cd71fd33b08ecd7fc047396714098c
pzz2011 <jyppzz@gmail.com>
2016-09-04 17:05:28 +0800

    test operator new(std::size_t sz)

--- operator/main.cc
+++ operator/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <new>
 using namespace std;
 
 class value {
@@ -25,6 +26,35 @@ private:
 };
 
 
+//class Integer {
+//private:
+//    long long data;
+//public:
+//    Integer(const long long data_): data(data_) {  }
+//    bool operator< (const Integer& lInteger, const Integer& rInteger){
+//        return lInteger.data < rInteger.data;
+//    }
+//    friend ostream& operator<< (ostream& os, const Integer& rInteger) {
+//        return os << "data = " << rInteger.data;
+//    }
+//};
+
+
+class NoMemory {
+public:
+    NoMemory() { 
+        std::cout << "NoMemory::NoMemory()" << std::endl;
+    }
+
+    void* operator new(std::size_t sz) throw(bad_alloc) {
+        std::cout << "NoMemory::operator new" << std::endl;
+        throw bad_alloc(); // Out of memory
+    }
+private:
+
+};
+
+
 class dog { 
 public:
     dog(const string& name_):name(name_){ }
@@ -45,6 +75,14 @@ int main() {
     value v2(1, 2, 4);
     value v3 = v2;
     std::cout << "v1 = " << v1 << std::endl;
-    assert(v1 == v2);
+//    assert(v1 == v2);
+    NoMemory* nm = 0;
+    try {
+        nm = new NoMemory;
+    } catch(bad_alloc) {
+        std::cerr << "Out of memory exception" << std::endl;
+    }
+    std::cout << "nm = " << nm << std::endl;
+
     return 0;
 }
