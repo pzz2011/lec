cbb201174ee4b1bc7905c235ae7f367c7a150057
pzz2011 <jyppzz@gmail.com>
2016-11-03 20:35:22 +0800

    usage of operator[] over object point ====> (*p3)[0] && template<type class, int dim>

--- operator/template_point.cc
+++ operator/template_point.cc
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<cstring>
 #include<cstdio>
-
+//#include<assert>
+#include<cassert>
 using namespace std;
 
 template< class type> 
@@ -22,12 +23,45 @@ private:
     type z_;
 };
 
+
+template< class type, int dim>
+class point_ {
+public: 
+    point_();
+    point_(type coords[dim]) { 
+        for (int index = 0; index < dim; ++index) { 
+            _coords[index] = coords[index];    
+        }
+    }
+
+    type& operator[] (int index) { 
+        assert (index < dim && index >= 0);
+        return _coords[index];
+    }
+
+    type operator[] (int index) const { 
+        // same as non-const instance
+        assert (index < dim && index >= 0);
+        return _coords[index];
+    }
+private:
+    type _coords[dim];
+};
+
 int main() { 
     point3d<int> p1(1,2,3);
     point3d<float>* p2 = new point3d<float>(4,5,6);
 
+    float data[3] = {1, 2, 3};
+    point_<float, 3>* p3 = new point_<float, 3>(data);
     cout << p2->x() << endl;
     cout << p2->y() << endl;
     cout << p2->z() << endl;
+
+
+    cout << "^^^^^^^^^^^^^^^^^^^^^^^" << endl;
+    cout << (*p3)[0]<< endl;
+//    cout << p3->[1]<< endl;
+//    cout << p3->[2]<< endl;
     return 0;
 }
