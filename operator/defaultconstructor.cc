6fb93c0a12c8e3a2f53d532a5835ac62e19b61cc
pzz2011 <jyppzz@gmail.com>
2016-11-04 15:50:43 +0800

    no matching function for call to ‘Foo::Foo()

---
+++ operator/defaultconstructor.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<string>
+using namespace std;
+
+class Foo {
+public: 
+//    Foo() {  }
+    Foo(int a) {}
+};
+
+//class Bar : public Foo { 
+//public: 
+////    Bar() {  str = "^_^";}
+//    const int& gstr() { return str; }
+//private: 
+//    Foo foo;
+//    int str;
+//}; 
+
+int main () { 
+//    Bar bar;
+//    if (bar.gstr()) cout << "^^^^^^^^^^^^^^^^^^^^^^^" << endl;
+//    cout << bar.gstr() << endl;
+    Foo foo;
+    return 0;
+}
